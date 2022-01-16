d16e4a9b416a2096ffb19a0a2d5249ea87a3e47f
connor <dmitry.jatsyshyn@gmail.com>
2022-01-16 20:23:38 +0200

    Added sliders values definition

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func sliderHeightTrigger(_ sender: UISlider) {
+        let height = String(format:"%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func sliderWeightTrigger(_ sender: UISlider) {
+        let weight = Int(sender.value)
+        weightLabel.text = "\(weight)Kg"
+    }
+    
+    
 
 }
 
