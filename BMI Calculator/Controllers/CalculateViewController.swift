9addfb9b85de2ae424ee456236ae0e27ea047179
connor <dmitry.jatsyshyn@gmail.com>
2022-01-27 23:40:29 +0200

    Added part of values passing to the second screen

---
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func sliderHeightTrigger(_ sender: UISlider) {
+        let height = String(format:"%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func sliderWeightTrigger(_ sender: UISlider) {
+        let weight = Int(sender.value)
+        weightLabel.text = "\(weight)Kg"
+    }
+    
+    @IBAction func calculateButton(_ sender: UIButton) {
+        
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmiResult = weight / (height * height) // else option pow(height, 2)
+        
+        print(height)
+        print(weight)
+        
+        print ("BMI Result: \(bmiResult)")
+        
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+        
+        
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = "0.0"
+        }
+    }
+    
+}
+
--- BMI Calculator/Controllers/ResultViewController.swift
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 
 class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
 
     @IBOutlet weak var bmiLabel: UILabel!
-    
     @IBOutlet weak var adviceLabel: UILabel!
     
     override func viewDidLoad() {
@@ -21,16 +22,11 @@ class ResultViewController: UIViewController {
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
+        
+        print(bmiValue)
+        
     }
     
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
