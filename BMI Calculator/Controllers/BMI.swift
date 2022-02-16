fca9dd2c8283dcdddcc378d20bcb7dee674023a7
connor <dmitry.jatsyshyn@gmail.com>
2022-02-17 00:00:30 +0200

    Added final finctionality

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,17 @@
+//
+//  BMI.swift
+// BMI Calculator (traning)
+//
+//  Created by Dmitry Y on 16/02/2022.
+//  Copyright © 2022 Dmitry Y. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
+
+
